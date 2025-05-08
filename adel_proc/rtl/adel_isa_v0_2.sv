0a2242eb6d29a3285cdf44d5e20ce96368cd5946
Lawrence Quizon <lawrence.quizon@eee.upd.edu.ph>
2025-05-08 09:52:40 +0200

    working (again)

--- adel_proc/rtl/adel_isa_v0_2.sv
+++ adel_proc/rtl/adel_isa_v0_2.sv
@@ -4,20 +4,19 @@ module adel (
     output reg [7:0] pc
 );
 reg signed [3:0][7:0] rf;
-wire [1:0] opc, dst, src1, src2;
+wire [1:0] opc, dst, src1, src2 = inst[1:0];
 wire w, rs;
-wire [7:0] imm;
-wire [7:0] bpc = pc + imm;
+wire [7:0] imm, bpc = pc + imm;
 wire [1:0] gl = {rf[src1] > 0, rf[src1] < 0};
-assign {w, opc, rs, dst, src1, imm} = inst;
-assign src2 = inst[1:0]; 
+assign {w, opc, rs, dst, src1, imm} = inst; 
 always @(posedge clk or negedge nrst) begin
     if (!nrst) begin pc <= 0; rf <= 0; end else begin
-    case ({w,opc})
-        4: rf[dst] <= rf[src1] + (rs ? rf[src2] : imm);
-        5: rf[dst] <= rf[src1] - (rs ? rf[src2] : imm);
-        6: rf[dst] <= rf[src1] & (rs ? rf[src2] : imm);
-        7: rf[dst] <= rf[src1] | (rs ? rf[src2] : imm);
+    case ({!w,opc})
+        0: rf[dst] <= rf[src1] + (rs ? rf[src2] : imm);
+        1: rf[dst] <= rf[src1] - (rs ? rf[src2] : imm);
+        2: rf[dst] <= rf[src1] & (rs ? rf[src2] : imm);
+        3: rf[dst] <= rf[src1] | (rs ? rf[src2] : imm);
+        default:;
     endcase
     pc <= !w & ( &opc & ^gl | opc == gl ) ? bpc : pc+1;
 end end
